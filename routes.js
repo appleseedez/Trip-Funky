d3a1632d7a8f6b1414aff42ff3f42e46ac0dbd08
cjj <chenjianjun571@gmail.com>
2016-04-05 16:12:16 +0800

    缓存API增加

--- routes.js
+++ routes.js
@@ -5,6 +5,12 @@ import { renderToString } from 'react-dom/server'
 import { ComponentsIndex, ComponentsSeo } from './components/config/components-index'
 import { Navigation } from './components/navigation.jsx'
 
+/** api的路由逻辑**/
+import advApi from './components/server/api/adv'
+import citySiteApi from './components/server/api/city'
+import pringlesApi from './components/server/api/pringles'
+import sampleApi from './components/server/api/sample'
+import suiteApi from './components/server/api/suite'
 
 const apiRouter = new Router({
   'prefix':'/api'
@@ -12,9 +18,34 @@ const apiRouter = new Router({
 apiRouter.get('/',function* apiIndex(next){
   yield next
   this.body={
-    '/':'Api Index'
+    '/':'Api Index',
+    '/api/adv/:position':'广告',
+    '/api/city':'旅拍城市列表',
+    '/api/attractions/:cityId':'根据旅拍城市Id获取景点',
+    '/api/pringles?cityId=旅拍城市ID&attractionsId=景点ID':'客片列表',
+    '/api/pringles/detail/:id':'客片详情',
+    '/api/sample?cityId=旅拍城市ID&attractionsId=景点ID':'样片列表',
+    '/api/sample/detail/:id':'样片详情',
+    '/api/suite?cityId=旅拍城市ID&attractionsId=景点ID':'套系列表',
+    '/api/suite/detail/:id':'套系详情',
+    'Adv,Sample,Pringles,Suite,CitySite,Attraction': '^_^'
   }
 })
+
+/** 把api的router在此生成 **/
+const apiRouterList = [
+  advApi,
+  citySiteApi,
+  pringlesApi,
+  sampleApi,
+  suiteApi
+]
+_.each(apiRouterList,(route,index)=>{
+  _.each(route,(value,key)=>{
+    apiRouter[key.split('+')[0]](key.split('+')[1], value)
+  })
+})
+
 export { apiRouter }
 
 
